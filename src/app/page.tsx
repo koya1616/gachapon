5dd9ef768e087ec065f07bf91eacc10651117bb5
koya1616 <koya1616.0622@gmail.com>
2025-04-18 10:33:32 +1000

    リファクタ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,65 +6,6 @@ import ProductGrid from "../components/ProductGrid";
 import Cart from "../components/Cart";
 
 export default function Home() {
-  const products: Product[] = [
-    {
-      id: 1,
-      name: "エコノミー機内食",
-      price: 777,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/airline_food.jpg",
-      quantity: 1,
-    },
-    {
-      id: 2,
-      name: "おいしい給食",
-      price: 1200,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/school_lunch.jpg",
-      quantity: 1,
-    },
-    {
-      id: 3,
-      name: "定番お惣菜",
-      price: 1000,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/teibann_osouzai.jpg",
-      quantity: 1,
-    },
-    {
-      id: 4,
-      name: "魔女の宅急便",
-      price: 10000,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/kikis_delivery_service.jpeg",
-      quantity: 1,
-    },
-    {
-      id: 5,
-      name: "鳥貴族",
-      price: 600,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/torikizoku.jpeg",
-      quantity: 1,
-    },
-    {
-      id: 6,
-      name: "遠足の思い出",
-      price: 300,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/bento_box_lunch.jpg",
-      quantity: 1,
-    },
-    {
-      id: 7,
-      name: "無印良品",
-      price: 400,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/muji.jpg",
-      quantity: 1,
-    },
-    {
-      id: 8,
-      name: "アイスクリーム",
-      price: 500,
-      image: "https://pub-099ffcea7b594ca6b20f500e6339a2c8.r2.dev/ice_cream.jpg",
-      quantity: 1,
-    },
-  ];
-
   const [cart, setCart] = useState<Product[]>([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
@@ -141,7 +82,7 @@ export default function Home() {
           onClearCart={clearCart}
         />
 
-        <ProductGrid products={products} onAddToCart={addToCart} />
+        <ProductGrid onAddToCart={addToCart} />
       </div>
     </>
   );
