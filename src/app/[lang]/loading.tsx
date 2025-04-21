0628d14ccf4e25302d815e3ed9a194ffb47e47a7
koya1616 <koya1616.0622@gmail.com>
2025-04-21 10:52:35 +1000

    Add loading component for product display with placeholder data

---
+++ src/app/[lang]/loading.tsx
@@ -0,0 +1,15 @@
+import Products from "@/components/Products";
+
+export default function Loading() {
+  const products = Array(4).fill({
+    id: 0,
+    name: "",
+    price: 0,
+    image: "product-loading.png",
+    quantity: 1,
+  }).map((product, index) => ({
+    ...product,
+    id: index + 1
+  }));
+  return <Products products={products} lang={"en"} />;
+}
