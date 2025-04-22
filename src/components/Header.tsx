03ebdde754135d287524a65156c502e2880ce208
koya1616 <koya1616.0622@gmail.com>
2025-04-22 14:08:34 +1000

    Refactor Header component to conditionally render cart button based on login path; improve user experience

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,11 @@ import Cart from "@/components/Cart";
 import LanguageDropdown from "@/components/LanguageDropdown";
 import { useCart } from "@/context/CartContext";
 import type { Lang } from "@/types";
+import { usePathname } from "next/navigation";
 
 export default function Header({ lang }: { lang: Lang }) {
+  const path = usePathname();
   const { cart, isCartOpen, totalPrice, toggleCart, closeCart, updateQuantity, removeFromCart, clearCart } = useCart();
-
   const cartItemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
@@ -26,31 +27,33 @@ export default function Header({ lang }: { lang: Lang }) {
         </div>
         <div className="py-4 flex justify-end items-center gap-4">
           <LanguageDropdown lang={lang} />
-          <button type="button" className="cursor-pointer" onClick={toggleCart}>
-            <div className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-                className="h-4 transition-all ease-in-out hover:scale-110"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                />
-              </svg>
-              {cartItemCount > 0 && (
-                <div className="absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-blue-600 text-[11px] font-medium text-white">
-                  {cartItemCount}
-                </div>
-              )}
-            </div>
-          </button>
+          {!path.includes("/login") && (
+            <button type="button" className="cursor-pointer" onClick={toggleCart}>
+              <div className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                  className="h-4 transition-all ease-in-out hover:scale-110"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                  />
+                </svg>
+                {cartItemCount > 0 && (
+                  <div className="absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-blue-600 text-[11px] font-medium text-white">
+                    {cartItemCount}
+                  </div>
+                )}
+              </div>
+            </button>
+          )}
         </div>
       </div>
 
