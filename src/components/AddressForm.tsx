0ae975bcf3ab1debabdaf7dae610e673843b938e
koya1616 <koya1616.0622@gmail.com>
2025-04-26 14:55:36 +1000

    Refactor AddressForm to use COUNTRY_LIST for country options, improving maintainability and scalability

--- src/components/AddressForm.tsx
+++ src/components/AddressForm.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState, useCallback, useMemo } from "react";
 import { useTranslation as t } from "@/lib/translations";
 import type { Address, Lang } from "@/types";
+import { COUNTRY_LIST } from "@/const/country";
 
 const AddressForm = ({ lang }: { lang: Lang }) => {
   const l = lang === "en" ? "en" : lang === "zh" ? "zh" : "ja";
@@ -107,54 +108,11 @@ const AddressForm = ({ lang }: { lang: Lang }) => {
               name="country"
               className="w-full p-2 border rounded"
             >
-              <option value="albania">Albania</option>
-              <option value="argentina">Argentina</option>
-              <option value="australia">Australia</option>
-              <option value="austria">Austria</option>
-              <option value="belgium">Belgium</option>
-              <option value="bosnia">Bosnia and Herzegovina</option>
-              <option value="brazil">Brazil</option>
-              <option value="bulgaria">Bulgaria</option>
-              <option value="canada">Canada</option>
-              <option value="china">China</option>
-              <option value="croatia">Croatia</option>
-              <option value="czech">Czech Republic</option>
-              <option value="denmark">Denmark</option>
-              <option value="finland">Finland</option>
-              <option value="france">France</option>
-              <option value="germany">Germany</option>
-              <option value="greece">Greece</option>
-              <option value="hungary">Hungary</option>
-              <option value="india">India</option>
-              <option value="indonesia">Indonesia</option>
-              <option value="ireland">Ireland</option>
-              <option value="italy">Italy</option>
-              <option value="japan">Japan</option>
-              <option value="korea">Korea</option>
-              <option value="malaysia">Malaysia</option>
-              <option value="mexico">Mexico</option>
-              <option value="netherlands">Netherlands</option>
-              <option value="newzealand">New Zealand</option>
-              <option value="norway">Norway</option>
-              <option value="philippines">Philippines</option>
-              <option value="poland">Poland</option>
-              <option value="portugal">Portugal</option>
-              <option value="romania">Romania</option>
-              <option value="russia">Russia</option>
-              <option value="serbia">Serbia</option>
-              <option value="singapore">Singapore</option>
-              <option value="slovakia">Slovakia</option>
-              <option value="slovenia">Slovenia</option>
-              <option value="southafrica">South Africa</option>
-              <option value="spain">Spain</option>
-              <option value="sweden">Sweden</option>
-              <option value="switzerland">Switzerland</option>
-              <option value="taiwan">Taiwan</option>
-              <option value="thailand">Thailand</option>
-              <option value="uk">United Kingdom</option>
-              <option value="ukraine">Ukraine</option>
-              <option value="us">United States</option>
-              <option value="vietnam">Vietnam</option>
+              {COUNTRY_LIST.map((country) => (
+                <option key={country.key} value={country.key}>
+                  {country.name}
+                </option>
+              ))}
             </select>
           )}
         </div>
