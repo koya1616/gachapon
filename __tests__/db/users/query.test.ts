c136857cd661038d095add1de7cccb198d63ae04
koya1616 <koya1616.0622@gmail.com>
2025-04-27 15:26:41 +1000

    Refactor user tests for clarity and organization

--- __tests__/db/users/query.test.ts
+++ __tests__/db/users/query.test.ts
@@ -3,38 +3,36 @@ import { createUser, executeQuery, findUserByEmail } from "@/lib/db";
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import { UserFactory } from "../../factory/user";
 
-describe("findUserByEmail", () => {
-  const testEmail = `${crypto.randomUUID().split("-")[0]}@example.com`;
-
-  let user: UserFactory;
-  beforeAll(async () => {
-    user = await UserFactory.create(testEmail);
-  });
-
+describe("Usersテーブルに関するテスト", () => {
   afterAll(async () => {
     await executeQuery("TRUNCATE TABLE users CASCADE");
   });
 
-  it("有効なメールアドレスの場合、ユーザー情報を返すべき", async () => {
-    const result = await findUserByEmail(testEmail);
-    expect(result?.id).toBe(user.id);
-    expect(result?.email).toBe(testEmail);
+  describe("findUserByEmail", () => {
+    const testEmail = `${crypto.randomUUID().split("-")[0]}@example.com`;
+
+    let user: UserFactory;
+    beforeAll(async () => {
+      user = await UserFactory.create(testEmail);
+    });
+
+    it("有効なメールアドレスの場合、ユーザー情報を返すべき", async () => {
+      const result = await findUserByEmail(testEmail);
+      expect(result?.id).toBe(user.id);
+      expect(result?.email).toBe(testEmail);
+    });
+
+    it("存在しないメールアドレスの場合、nullを返すべき", async () => {
+      const result = await findUserByEmail("invalid@example.com");
+      expect(result).toBeNull();
+    });
   });
 
-  it("存在しないメールアドレスの場合、nullを返すべき", async () => {
-    const result = await findUserByEmail("invalid@example.com");
-    expect(result).toBeNull();
-  });
-});
-
-describe("createUser", () => {
-  afterAll(async () => {
-    await executeQuery("TRUNCATE TABLE users CASCADE");
-  });
-
-  it("有効なメールアドレスの場合、ユーザー情報を返すべき", async () => {
-    const email = `${crypto.randomUUID().split("-")[0]}@example.com`;
-    const user = await createUser(email);
-    expect(user?.email).toBe(email);
+  describe("createUser", () => {
+    it("有効なメールアドレスの場合、ユーザー情報を返すべき", async () => {
+      const email = `${crypto.randomUUID().split("-")[0]}@example.com`;
+      const user = await createUser(email);
+      expect(user?.email).toBe(email);
+    });
   });
 });
