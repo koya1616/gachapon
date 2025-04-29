ca7b57c15bb63b24d93fdc91384365dbc7d4c54b
koya1616 <koya1616.0622@gmail.com>
2025-04-29 22:18:30 +1000

    Refactor date tests to improve structure and clarity

--- __tests__/date/date.test.ts
+++ __tests__/date/date.test.ts
@@ -1,7 +1,7 @@
 import { formatDate } from "@/lib/date";
 import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 
-describe("Date Module", () => {
+describe("formatDate", () => {
   beforeEach(() => {
     const fixedDate = new Date("2023-01-01T00:00:00Z");
     vi.setSystemTime(fixedDate);
@@ -12,30 +12,23 @@ describe("Date Module", () => {
     vi.unstubAllEnvs();
   });
 
-  describe("formatDate", () => {
-    it("正しいタイムスタンプを渡すと、正しい日付形式で返すこと", () => {
-      const formattedDate = formatDate(1672531199000);
-      expect(formattedDate).toBe("2023/1/1 10:59:59");
-    });
+  it("無効なタイムスタンプを渡すと、Invalid Dateを返すこと", () => {
+    const invalidTimestamp = "invalid";
+    const formattedDate = formatDate(invalidTimestamp);
+    expect(formattedDate).toBe("Invalid Date");
+  });
 
-    it("無効なタイムスタンプを渡すと、Invalid Dateを返すこと", () => {
-      const invalidTimestamp = "invalid";
-      const formattedDate = formatDate(invalidTimestamp);
-      expect(formattedDate).toBe("Invalid Date");
-    });
+  it("JST (Asia/Tokyo)のタイムゾーンで正しくフォーマットされること", () => {
+    vi.stubEnv("TZ", "Asia/Tokyo");
 
-    it("JST (Asia/Tokyo)のタイムゾーンで正しくフォーマットされること", () => {
-      vi.stubEnv("TZ", "Asia/Tokyo");
+    const formattedDate = formatDate(1672531199000);
+    expect(formattedDate).toBe("2023/1/1 8:59:59");
+  });
 
-      const formattedDate = formatDate(1672531199000);
-      expect(formattedDate).toBe("2023/1/1 8:59:59");
-    });
+  it("UTC (GMT)のタイムゾーンで正しくフォーマットされること", () => {
+    vi.stubEnv("TZ", "UTC");
 
-    it("UTC (GMT)のタイムゾーンで正しくフォーマットされること", () => {
-      vi.stubEnv("TZ", "UTC");
-
-      const formattedDate = formatDate(1672531199000);
-      expect(formattedDate).toBe("2022/12/31 23:59:59");
-    });
+    const formattedDate = formatDate(1672531199000);
+    expect(formattedDate).toBe("2022/12/31 23:59:59");
   });
 });
