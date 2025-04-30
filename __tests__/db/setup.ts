8652aa8666e309f20e30892b54a2eb710da2c1b0
koya1616 <koya1616.0622@gmail.com>
2025-04-30 18:50:56 +1000

    Refactor setup function to adjust table truncation order in test setup

--- __tests__/db/setup.ts
+++ __tests__/db/setup.ts
@@ -2,9 +2,12 @@ import { executeQuery } from "@/lib/db";
 import type { TestProject } from "vitest/node";
 
 export default async function setup(project: TestProject) {
-  await executeQuery("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
+  await executeQuery("TRUNCATE TABLE addresses RESTART IDENTITY CASCADE");
+  await executeQuery("TRUNCATE TABLE lottery_events RESTART IDENTITY CASCADE");
+  await executeQuery("TRUNCATE TABLE lottery_products RESTART IDENTITY CASCADE");
   await executeQuery("TRUNCATE TABLE products RESTART IDENTITY CASCADE");
+  await executeQuery("TRUNCATE TABLE payment_products RESTART IDENTITY CASCADE");
   await executeQuery("TRUNCATE TABLE paypay_payments RESTART IDENTITY CASCADE");
   await executeQuery("TRUNCATE TABLE shipments RESTART IDENTITY CASCADE");
-  await executeQuery("TRUNCATE TABLE payment_products RESTART IDENTITY CASCADE");
+  await executeQuery("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
 }
