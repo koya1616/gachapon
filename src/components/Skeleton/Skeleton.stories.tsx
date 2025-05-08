360254d188774c247f5e0f9fe5a20d0e4ae3e672
koya1616 <koya1616.0622@gmail.com>
2025-05-08 21:11:12 +1000

    feat: Add Skeleton component stories for Storybook integration

---
+++ src/components/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Skeleton from "./Skeleton";
+
+const meta = {
+  title: "Components/Skeleton",
+  component: Skeleton,
+  tags: ["autodocs"],
+  argTypes: {
+    h: {
+      control: {
+        type: "number",
+      },
+    },
+  },
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    h: 10,
+  },
+};
