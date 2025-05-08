3c45f3c87d5e80500a1339b17813e4a4c3cf331c
koya1616 <koya1616.0622@gmail.com>
2025-05-09 09:41:34 +1000

    feat: Add Header component stories for Storybook integration

---
+++ src/app/[lang]/_components/Header/Header.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { HeaderView } from "./Header";
+import { mockProducts } from "@/mocks/data";
+
+const meta = {
+  title: "Components/Header",
+  component: HeaderView,
+  parameters: {
+    layout: "fullscreen",
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof HeaderView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    lang: "ja",
+    showCart: true,
+    cartItemCount: 3,
+    isCartOpen: false,
+    cart: mockProducts.slice(0, 3).map((product) => ({
+      ...product,
+      quantity: 1,
+    })),
+    totalPrice: 1500,
+    toggleCart: () => console.log("toggleCart"),
+    closeCart: () => console.log("closeCart"),
+    updateQuantity: (productId: number, quantity: number) => console.log("updateQuantity", productId, quantity),
+    removeFromCart: (productId: number) => console.log("removeFromCart", productId),
+    clear_cart: () => console.log("clear_cart"),
+    handleLogoClick: () => console.log("handleLogoClick"),
+    handleAccountClick: () => console.log("handleAccountClick"),
+  },
+};
+
+export const CartOpen: Story = {
+  args: {
+    ...Default.args,
+    isCartOpen: true,
+  },
+};
+
+export const EmptyCart: Story = {
+  args: {
+    ...Default.args,
+    cartItemCount: 0,
+    cart: [],
+    totalPrice: 0,
+  },
+};
+
+export const WithoutCartButton: Story = {
+  args: {
+    ...Default.args,
+    showCart: false,
+  },
+};
