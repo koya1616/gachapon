113f2a35d2379996c4bbafeec60b7d6f02d02f4b
koya1616 <koya1616.0622@gmail.com>
2025-05-09 19:53:12 +1000

    feat: Add AccountPageView stories for displaying user orders

---
+++ src/app/[lang]/account/_components/AccountPageView/AccountPageView.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import AccountPageView from "./AccountPageView";
+import { createOrder } from "@/mocks/data";
+
+const meta = {
+  title: "Components/Account/Page",
+  component: AccountPageView,
+  parameters: {
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof AccountPageView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const NoOrders: Story = {
+  args: {
+    orders: [],
+    l: "ja",
+  },
+};
+
+export const WithOrders: Story = {
+  args: {
+    orders: [
+      createOrder(),
+      createOrder("2023-01-02T00:00:00.000Z"),
+      createOrder("2023-01-02T00:00:00.000Z", "2023-01-03T00:00:00.000Z"),
+      createOrder(null, null, "2023-01-04T00:00:00.000Z"),
+      createOrder(null, null, null, "2023-01-05T00:00:00.000Z"),
+    ],
+    l: "ja",
+  },
+};
