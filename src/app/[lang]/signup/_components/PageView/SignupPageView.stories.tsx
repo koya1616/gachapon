8c4247c7c16cc7dc97d88b5df677d2abeb1b3cbc
koya1616 <koya1616.0622@gmail.com>
2025-05-09 21:27:55 +1000

    feat: Add SignupPageView stories for Storybook integration

---
+++ src/app/[lang]/signup/_components/PageView/SignupPageView.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import SignupPageView from "./SignupPageView";
+
+const meta = {
+  title: "Pages/Signup",
+  component: SignupPageView,
+  parameters: {
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof SignupPageView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    l: "ja",
+  },
+};
