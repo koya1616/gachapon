d33931a573b89805b4a6e8cdc9025168ceaa9c12
koya1616 <koya1616.0622@gmail.com>
2025-05-11 09:47:18 +1000

    refactor: Update IconDisplay layout and correct story title

--- src/components/Icons/Icon.stories.tsx
+++ src/components/Icons/Icon.stories.tsx
@@ -12,23 +12,21 @@ import {
 
 const IconDisplay = () => {
   return (
-    <div className="p-6 rounded-lg">
-      <div className="grid grid-cols-3 gap-8 md:grid-cols-4 lg:grid-cols-8">
-        <MenuIcon className="h-6 w-6" />
-        <CloseIcon className="h-6 w-6" />
-        <AccountIcon />
-        <CartIcon />
-        <GoogleIcon />
-        <CreditCardIcon />
-        <LanguageIcon />
-        <PayPayIcon />
-      </div>
+    <div className="grid grid-cols-3 gap-8 md:grid-cols-4 lg:grid-cols-8">
+      <MenuIcon className="h-6 w-6" />
+      <CloseIcon className="h-6 w-6" />
+      <AccountIcon />
+      <CartIcon />
+      <GoogleIcon />
+      <CreditCardIcon />
+      <LanguageIcon />
+      <PayPayIcon />
     </div>
   );
 };
 
 const meta = {
-  title: "Icons",
+  title: "Components/Icons",
   component: IconDisplay,
   tags: ["autodocs"],
 } satisfies Meta<typeof IconDisplay>;
