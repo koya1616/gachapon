27dbf6b3c2da6abb29912ee304ca8131d6e78f63
koya1616 <koya1616.0622@gmail.com>
2025-05-10 12:26:27 +1000

    feat: Add MenuIcon and CloseIcon components for improved header functionality

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "../globals.css";
+import { CloseIcon, MenuIcon } from "@/components/Icons";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -59,26 +60,8 @@ const AdminHeader = () => {
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
               >
                 <span className="sr-only">メニューを開く</span>
-                <svg
-                  className={`${isMenuOpen ? "hidden" : "block"} h-6 w-6`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-                <svg
-                  className={`${isMenuOpen ? "block" : "hidden"} h-6 w-6`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
+                <MenuIcon className={`${isMenuOpen ? "hidden" : "block"} h-6 w-6`} />
+                <CloseIcon className={`${isMenuOpen ? "block" : "hidden"} h-6 w-6`} />
               </button>
             </div>
           </div>
